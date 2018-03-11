e2e623357ba93f2e39a84954864c48dccc394c9f
Dillon Huff <dillonhuff@gmail.com>
2018-03-11 13:06:21 -0700

    Creating a real loader

--- cpu_main.cpp
+++ cpu_main.cpp
@@ -5,10 +5,23 @@
 
 using namespace std;
 
-#define MEM top->cpu__DOT__main_mem__DOT__mem
+#define MEM cpu__DOT__main_mem__DOT__mem
+
+uint32_t tiny_CPU_no_op_instr() {
+  return 0;
+}
+
+void load_increment_program(const int mem_depth, Vcpu* const top) {
+  for (int i = 0; i < mem_depth; i++) {
+    uint32_t no_op = tiny_CPU_no_op_instr();
+    top->MEM[i] = no_op;
+  }
+}
 
 int main(const int argc, char** argv) {
   Vcpu* top = new Vcpu();
+
+  load_increment_program(2048, top);
 
   top->rst = 0;
   top->clk = 0;
