89656631a99496e11e3f27f5900824f8154fa94c
Dillon Huff <dillonhuff@gmail.com>
2018-03-22 22:13:34 -0700

    Added a stall condition

--- stall_detector.v
+++ stall_detector.v
@@ -4,6 +4,16 @@ module stall_detector(output [0:0] stall,
                       input [31:0] execute_stage_instruction,
                       input [31:0] memory_stage_instruction);
 
-   assign stall = 1'b0;
+   wire                            issue_reg_holds_no_op;
+   assign issue_reg_holds_no_op = issue_reg_output == 32'h0;
+
+   wire issue_reg_instr_in_pipe;
+   assign issue_reg_instr_in_pipe = (issue_reg_output == decode_stage_instruction) ||
+                                    (issue_reg_output == execute_stage_instruction) ||
+                                    (issue_reg_output == memory_stage_instruction);
+   
+
+   assign stall = !issue_reg_holds_no_op && issue_reg_instr_in_pipe;
+//1'b0;
 
 endmodule
