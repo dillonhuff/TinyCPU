80adc47c3a92d271956eb35eff176793dea8eade
Dillon Huff <dillonhuff@gmail.com>
2018-03-26 21:52:48 -0700

    Breaking the design up into stages

--- cpu_pipelined_basic.v
+++ cpu_pipelined_basic.v
@@ -231,64 +231,65 @@ module cpu_pipelined_basic(input clk,
                                    .D(decode_ireg_input),
                                    .Q(decode_ireg_out));
 
-   wire [4:0]  ireg_alu_operation;
-   assign ireg_alu_operation = decode_ireg_out[11:7];
+   // STAGE EXE
+   stage_exe execute(.clk(clk),
+                     .rst(rst),
+                     .instruction_in(decode_ireg_out),
+                     .register_a_value(read_data_0),
+                     .register_b_value(read_data_1));
    
+   // // Arithmetic logic unit
+   // wire [31:0] alu_result_reg_input;
+   // wire [31:0] alu_result;
 
-   // STAGE EXE   
-   // Arithmetic logic unit
-   wire [31:0] alu_result_reg_input;
-   wire [31:0] alu_result;
+   // wire [31:0] alu_in0;
+   // wire [31:0] alu_in1;
+   // wire [4:0]  alu_op_select;
 
-   wire [31:0] alu_in0;
-   wire [31:0] alu_in1;
-   wire [4:0]  alu_op_select;
+   // alu_control alu_ctrl(.alu_operation(ireg_alu_operation),
+
+   //                      .reg_value_0(read_data_0),
+   //                      .reg_value_1(read_data_1),
+
+   //                      // Outputs sent to ALU
+   //                      .alu_in0(alu_in0),
+   //                      .alu_in1(alu_in1),
+   //                      .alu_op_select(alu_op_select)
+   //                      );
+
+   // alu ALU(.in0(alu_in0),
+   //         .in1(alu_in1),
+   //         .op_select(alu_op_select),
+   //         .out(alu_result_reg_input));
+
+   // // Execution stage result pipeline register
+   // reg_async_reset alu_result_reg(.clk(clk),
+   //                                .rst(rst),
+   //                                .en(1'b1),
+   //                                .D(alu_result_reg_input),
+   //                                .Q(alu_result));
+
+   // wire [31:0] execute_ireg_out;
+   // reg_async_reset end_execute_ireg(.clk(clk),
+   //                                  .rst(rst),
+   //                                  .en(1'b1),
+   //                                  .D(decode_ireg_out),
+   //                                  .Q(execute_ireg_out));
+
+   // wire [31:0] read_data_0_exe;
+   // wire [31:0] read_data_1_exe;
    
-   
-   alu_control alu_ctrl(.alu_operation(ireg_alu_operation), //.alu_operation(alu_operation),
+   // reg_async_reset reg_file_data_0_e(.clk(clk),
+   //                                   .rst(rst),
+   //                                   .en(1'b1),
+   //                                   .D(read_data_0),
+   //                                   .Q(read_data_0_exe));
 
-                        .reg_value_0(read_data_0),
-                        .reg_value_1(read_data_1),
-
-                        // Outputs sent to ALU
-                        .alu_in0(alu_in0),
-                        .alu_in1(alu_in1),
-                        .alu_op_select(alu_op_select)
-                        );
-   
-   alu ALU(.in0(alu_in0),
-           .in1(alu_in1),
-           .op_select(alu_op_select),
-           .out(alu_result_reg_input));
-
-   // Execution stage result pipeline register
-   reg_async_reset alu_result_reg(.clk(clk),
-                                  .rst(rst),
-                                  .en(1'b1),
-                                  .D(alu_result_reg_input),
-                                  .Q(alu_result));
-
-   wire [31:0] execute_ireg_out;
-   reg_async_reset end_execute_ireg(.clk(clk),
-                                    .rst(rst),
-                                    .en(1'b1),
-                                    .D(decode_ireg_out),
-                                    .Q(execute_ireg_out));
-
-   wire [31:0] read_data_0_exe;
-   wire [31:0] read_data_1_exe;
-   
-   reg_async_reset reg_file_data_0_e(.clk(clk),
-                                     .rst(rst),
-                                     .en(1'b1),
-                                     .D(read_data_0),
-                                     .Q(read_data_0_exe));
-
-   reg_async_reset reg_file_data_1_e(.clk(clk),
-                                     .rst(rst),
-                                     .en(1'b1),
-                                     .D(read_data_1),
-                                     .Q(read_data_1_exe));
+   // reg_async_reset reg_file_data_1_e(.clk(clk),
+   //                                   .rst(rst),
+   //                                   .en(1'b1),
+   //                                   .D(read_data_1),
+   //                                   .Q(read_data_1_exe));
    
    // STAGE MEMORY
 
