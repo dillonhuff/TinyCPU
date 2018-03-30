f705f843a27e1a8c5f87f3d0c22977d703973cb1
Dillon Huff <dillonhuff@gmail.com>
2018-03-30 16:15:42 -0700

    Now have full decode stage passing tests

--- cpu_pipelined_basic.v
+++ cpu_pipelined_basic.v
@@ -26,6 +26,7 @@ module cpu_pipelined_basic(input clk,
 
    // Stall logic
    wire stall;
+   wire [4:0] current_instruction_type;
    
    stall_detector
      stall_detect(.stall(stall),
@@ -73,103 +74,103 @@ module cpu_pipelined_basic(input clk,
 
    wire [31:0]      issue_register_input;
    assign issue_register_input = squash_issue ? 32'h0 : main_mem_read_data_0;
+
+   wire [31:0] current_instruction;
    reg_async_reset #(.width(32)) issue_register(.clk(clk),
                                                 .rst(rst),
                                                 .en(issue_reg_en),
                                                 .D(issue_register_input),
                                                 .Q(current_instruction));
 
-   wire [31:0] current_instruction;
-   wire [4:0] current_instruction_type;
-
-   
-   wire [4:0] load_imm_reg;
-   wire [31:0] load_imm_data;
-   
-
-   wire [4:0]  load_mem_reg;
-   wire [4:0] load_mem_addr_reg;
-
-   wire [4:0] alu_op_reg_0;
-   wire [4:0] alu_op_reg_1;
-   wire [4:0] alu_op_reg_res;
-   wire [4:0] alu_op_reg_res_wb;
-   wire [4:0] alu_operation;
-
-   wire [4:0] store_data_reg;
-   wire [4:0] store_addr_reg;
-
-   wire [4:0] jump_condition_reg;
-   wire [4:0] jump_address_reg;
-   
-   basic_pipeline_decoder
-     instruction_decode(.instruction(current_instruction),
-
-                        // Outputs
-                        .instruction_type(current_instruction_type),
-                        .load_imm_reg(load_imm_reg),
-                        .load_imm_data(load_imm_data),
-
-                        .load_mem_addr_reg(load_mem_addr_reg),
-                        .load_mem_reg(load_mem_reg),
-
-                        .store_data_reg(store_data_reg),
-                        .store_addr_reg(store_addr_reg),
-
-                        .alu_op_reg_0(alu_op_reg_0),
-                        .alu_op_reg_1(alu_op_reg_1),
-                        .alu_operation(alu_operation),
-
-                        .jump_condition_reg(jump_condition_reg),
-                        .jump_address_reg(jump_address_reg)
-                        );
-   
-
-   // Register file
-   wire [4:0] read_reg_0;
-   wire [4:0] read_reg_1;
-   wire [4:0] write_reg;
-
-   wire [31:0] reg_file_write_data;
 
    wire [31:0]        read_data_0;
    wire [31:0]        read_data_1;
 
-   wire        reg_file_write_en;
-
+   // wire [4:0] read_reg_0;
+   // wire [4:0] read_reg_1;
    
-   pipelined_basic_register_file_control
-     reg_file_ctrl(
-                   // Control info
-                   .decode_instruction_type(current_instruction_type),
-                   .write_back_instruction_type(wb_instruction_type),
+   // wire [4:0] current_instruction_type;
+   
+   // wire [4:0] load_imm_reg;
+   // wire [31:0] load_imm_data;
+   
 
-                   .load_imm_reg(write_back_load_imm_reg),
-                   .load_imm_data(write_back_load_imm_data),
+   // wire [4:0]  load_mem_reg;
+   // wire [4:0] load_mem_addr_reg;
 
-                   .load_mem_reg(write_back_load_mem_reg),
-                   .load_mem_data(write_back_register_input),
-                   .load_mem_addr_reg(load_mem_addr_reg),
+   // wire [4:0] alu_op_reg_0;
+   // wire [4:0] alu_op_reg_1;
+   // wire [4:0] alu_op_reg_res;
+   // wire [4:0] alu_op_reg_res_wb;
+   // wire [4:0] alu_operation;
 
-                   // These should come from the write back register
-                   .store_addr_reg(store_addr_reg),
-                   .store_data_reg(store_data_reg),
+   // wire [4:0] store_data_reg;
+   // wire [4:0] store_addr_reg;
 
-                   .alu_op_reg_0(alu_op_reg_0),
-                   .alu_op_reg_1(alu_op_reg_1),
-                   .alu_op_reg_res(alu_op_reg_res_wb),
-                   .alu_result(write_back_register_input),
+   // wire [4:0] jump_condition_reg;
+   // wire [4:0] jump_address_reg;
+   
+   // basic_pipeline_decoder
+   //   instruction_decode(.instruction(current_instruction),
 
-                   // My guess is that jump condition registers need to be updated
-                   .jump_condition_reg(jump_condition_reg),
-                   .jump_address_reg(jump_address_reg),
+   //                      // Outputs
+   //                      .instruction_type(current_instruction_type),
+   //                      .load_imm_reg(load_imm_reg),
+   //                      .load_imm_data(load_imm_data),
 
-                   // Inputs to the register file
-                   .write_address(write_reg),
-                   .write_data(reg_file_write_data),
-                   .write_enable(reg_file_write_en),
-                   .read_reg_0(read_reg_0),
-                   .read_reg_1(read_reg_1));
+   //                      .load_mem_addr_reg(load_mem_addr_reg),
+   //                      .load_mem_reg(load_mem_reg),
+
+   //                      .store_data_reg(store_data_reg),
+   //                      .store_addr_reg(store_addr_reg),
+
+   //                      .alu_op_reg_0(alu_op_reg_0),
+   //                      .alu_op_reg_1(alu_op_reg_1),
+   //                      .alu_operation(alu_operation),
+
+   //                      .jump_condition_reg(jump_condition_reg),
+   //                      .jump_address_reg(jump_address_reg)
+   //                      );
+
+   // // Register file
+   // wire [4:0] write_reg;
+
+   // wire [31:0] reg_file_write_data;
+
+   // wire        reg_file_write_en;
+
+   // pipelined_basic_register_file_control
+   //   reg_file_ctrl(
+   //                 // Control info
+   //                 .decode_instruction_type(current_instruction_type),
+   //                 .write_back_instruction_type(wb_instruction_type),
+
+   //                 .load_imm_reg(write_back_load_imm_reg),
+   //                 .load_imm_data(write_back_load_imm_data),
+
+   //                 .load_mem_reg(write_back_load_mem_reg),
+   //                 .load_mem_data(write_back_register_input),
+   //                 .load_mem_addr_reg(load_mem_addr_reg),
+
+   //                 // These should come from the write back register
+   //                 .store_addr_reg(store_addr_reg),
+   //                 .store_data_reg(store_data_reg),
+
+   //                 .alu_op_reg_0(alu_op_reg_0),
+   //                 .alu_op_reg_1(alu_op_reg_1),
+   //                 .alu_op_reg_res(alu_op_reg_res_wb),
+   //                 .alu_result(write_back_register_input),
+
+   //                 // My guess is that jump condition registers need to be updated
+   //                 .jump_condition_reg(jump_condition_reg),
+   //                 .jump_address_reg(jump_address_reg),
+
+   //                 // Inputs to the register file
+   //                 .write_address(write_reg),
+   //                 .write_data(reg_file_write_data),
+   //                 .write_enable(reg_file_write_en),
+   //                 .read_reg_0(read_reg_0),
+   //                 .read_reg_1(read_reg_1));
 
    wire [31:0] decode_ireg_out;
 
@@ -177,21 +178,30 @@ module cpu_pipelined_basic(input clk,
                              .clk(clk),
                              .rst(rst),
 
-                             .read_reg_0(read_reg_0),
-                             .read_reg_1(read_reg_1),
+                             // .read_reg_0(read_reg_0),
+                             // .read_reg_1(read_reg_1),
 
-                             .reg_file_write_data(reg_file_write_data),
-                             .reg_file_write_en(reg_file_write_en),
+                             .alu_op_reg_res_wb(alu_op_reg_res_wb),
 
-                             .write_reg(write_reg),
+                             //.reg_file_write_data(reg_file_write_data),
+                             //.reg_file_write_en(reg_file_write_en),
+
+                             //.write_reg(write_reg),
 
                              .stall(stall),
                              .current_instruction(current_instruction),
 
+                             .wb_instruction_type(wb_instruction_type),
+                             .write_back_load_imm_reg(write_back_load_imm_reg),
+                             .write_back_load_imm_data(write_back_load_imm_data),
+                             .write_back_load_mem_reg(write_back_load_mem_reg),
+                             .write_back_register_input(write_back_register_input),
+
                              // Outputs
                              .read_data_0(read_data_0),
                              .read_data_1(read_data_1),
-                             .decode_ireg_out(decode_ireg_out));
+                             .decode_ireg_out(decode_ireg_out),
+                             .current_instruction_type(current_instruction_type));
    
    // STAGE EXE
    wire [31:0] execute_ireg_out;
@@ -251,6 +261,7 @@ module cpu_pipelined_basic(input clk,
    wire [4:0] write_back_load_imm_reg;
    wire [31:0] write_back_load_imm_data;
    wire [4:0]  wb_instruction_type;
+   wire [4:0] alu_op_reg_res_wb;
    
    stage_write_back write_back(.instruction_in(memory_ireg_out),
                                .write_back_load_mem_reg(write_back_load_mem_reg),
--- stage_decode.v
+++ stage_decode.v
@@ -1,26 +1,122 @@
 module stage_decode(input clk,
                     input         rst,
 
-                    // input [31:0]  reg_file_data_0,
-                    // input [31:0]  reg_file_data_1,
+                    // input [4:0]   read_reg_0,
+                    // input [4:0]   read_reg_1,
 
-                    input [4:0]   read_reg_0,
-                    input [4:0]   read_reg_1,
+                    //input [4:0]   write_reg,
+                    //input [31:0]  reg_file_write_data,
+                    //input         reg_file_write_en,
 
-                    input [4:0]   write_reg,
-                    input [31:0]  reg_file_write_data,
-                    input         reg_file_write_en,
+                    input [4:0]   alu_op_reg_res_wb,
 
                     input         stall,
 
                     input [31:0]  current_instruction,
 
+
+                    input [4:0]   wb_instruction_type,
+                    input [4:0]   write_back_load_imm_reg,
+                    input [31:0]  write_back_load_imm_data,
+                    input [4:0]   write_back_load_mem_reg,
+                    input [31:0]   write_back_register_input,
+                    
                     // Outputs
                     output [31:0] read_data_0,
                     output [31:0] read_data_1,
-                    output [31:0] decode_ireg_out
+                    output [31:0] decode_ireg_out,
+                    output [4:0]  current_instruction_type
                     );
 
+   //wire [4:0] current_instruction_type;
+
+
+   /* verilator lint_off UNUSED */   
+   wire [4:0] load_imm_reg;
+   wire [31:0] load_imm_data;
+   
+
+   wire [4:0]  load_mem_reg;
+   wire [4:0] load_mem_addr_reg;
+
+   wire [4:0] alu_op_reg_0;
+   wire [4:0] alu_op_reg_1;
+   wire [4:0] alu_op_reg_res;
+   wire [4:0] alu_operation;
+
+   wire [4:0] store_data_reg;
+   wire [4:0] store_addr_reg;
+
+   wire [4:0] jump_condition_reg;
+   wire [4:0] jump_address_reg;
+   
+   basic_pipeline_decoder
+     instruction_decode(.instruction(current_instruction),
+
+                        // Outputs
+                        .instruction_type(current_instruction_type),
+                        .load_imm_reg(load_imm_reg),
+                        .load_imm_data(load_imm_data),
+
+                        .load_mem_addr_reg(load_mem_addr_reg),
+                        .load_mem_reg(load_mem_reg),
+
+                        .store_data_reg(store_data_reg),
+                        .store_addr_reg(store_addr_reg),
+
+                        .alu_op_reg_0(alu_op_reg_0),
+                        .alu_op_reg_1(alu_op_reg_1),
+                        .alu_operation(alu_operation),
+
+                        .jump_condition_reg(jump_condition_reg),
+                        .jump_address_reg(jump_address_reg)
+                        );
+
+   // Register file
+   wire [4:0] read_reg_0;
+   wire [4:0] read_reg_1;
+   wire [4:0] write_reg;
+
+   wire [31:0] reg_file_write_data;
+
+   wire [31:0]        read_data_0;
+   wire [31:0]        read_data_1;
+
+   wire        reg_file_write_en;
+
+   pipelined_basic_register_file_control
+     reg_file_ctrl(
+                   // Control info
+                   .decode_instruction_type(current_instruction_type),
+                   .write_back_instruction_type(wb_instruction_type),
+
+                   .load_imm_reg(write_back_load_imm_reg),
+                   .load_imm_data(write_back_load_imm_data),
+
+                   .load_mem_reg(write_back_load_mem_reg),
+                   .load_mem_data(write_back_register_input),
+                   .load_mem_addr_reg(load_mem_addr_reg),
+
+                   // These should come from the write back register
+                   .store_addr_reg(store_addr_reg),
+                   .store_data_reg(store_data_reg),
+
+                   .alu_op_reg_0(alu_op_reg_0),
+                   .alu_op_reg_1(alu_op_reg_1),
+                   .alu_op_reg_res(alu_op_reg_res_wb),
+                   .alu_result(write_back_register_input),
+
+                   // My guess is that jump condition registers need to be updated
+                   .jump_condition_reg(jump_condition_reg),
+                   .jump_address_reg(jump_address_reg),
+
+                   // Inputs to the register file
+                   .write_address(write_reg),
+                   .write_data(reg_file_write_data),
+                   .write_enable(reg_file_write_en),
+                   .read_reg_0(read_reg_0),
+                   .read_reg_1(read_reg_1));
+   
    wire [31:0] reg_file_data_0;
    wire [31:0] reg_file_data_1;
 
