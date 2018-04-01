8d1c6ccc02a813d9bb276d2ccdca950015beb6dd
Dillon Huff <dillonhuff@gmail.com>
2018-03-31 19:51:28 -0700

    Working on read detector

--- RAW_dependence_detector.v
+++ RAW_dependence_detector.v
@@ -5,9 +5,33 @@ module RAW_dependence_detector(/* verilator lint_off UNUSED */
 
                                output       has_RAW_dependence);
 
-   // read_detector i1_reads(.instr(i1));
-   // write_detector i0_write(.instr(i0));
+   wire                                     i1_reads_0;
+   wire                                     i1_reads_1;
 
-   assign has_RAW_dependence = 1'h0;
+   wire [4:0]                               i1_read_reg_0;
+   wire [4:0]                               i1_read_reg_1;
+   
+   read_detector i1_reads(.instr(i1),
+                          
+                          .reads_0(i1_reads_0),
+                          .reads_1(i1_reads_1),
+
+                          .read_reg_0(i1_read_reg_0),
+                          .read_reg_1(i1_read_reg_1));
+
+   wire                                     i0_writes;
+   wire [4:0]                               i0_write_reg;
+   
+   write_detector i0_write(.instr(i0),
+                           
+                           .writes(i0_writes),
+
+                           .write_reg(i0_write_reg));
+   
+
+   assign has_RAW_dependence = i0_writes &&
+                               (((i0_write_reg == i1_read_reg_0) && i1_reads_0) ||
+                                ((i0_write_reg == i1_read_reg_1) && i1_reads_1));
+   
    
 endmodule
---
+++ read_detector.v
@@ -0,0 +1,8 @@
+module read_detector(input [31:0] instr,
+                     
+                     output reads_0,
+                     output reads_1,
+
+                     output [4:0] read_reg_0,
+                     output [4:0] read_reg_1);
+endmodule
