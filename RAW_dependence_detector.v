6f30f0a069a54a908e692779a8a776b7d7104584
Dillon Huff <dillonhuff@gmail.com>
2018-03-31 19:28:26 -0700

    Starting to work on RAW based stall detection

---
+++ RAW_dependence_detector.v
@@ -0,0 +1,3 @@
+module RAW_dependence_detector(input [31:0] );
+   
+endmodule
--- stall_detector.v
+++ stall_detector.v
@@ -7,12 +7,39 @@ module stall_detector(output [0:0] stall,
 
    // New stall condition: Stall if:
    // 1. A branch has gotten past the issue stage. Q: Do I need to squash?
-   // 2. 
+   // 2. An instruction past issue register writes a register that an
+   //    instruction in the issue register reads
 
-   wire any_instr_in_pipe = (32'h0 != decode_stage_instruction) ||
-        (32'h0 != execute_stage_instruction) ||
-        (32'h0 != memory_stage_instruction);
+   wire                            decode_is_branch;
+   branch_detector decode_b(.instr(decode_stage_instruction),
+                            .is_branch(decode_is_branch));
 
-   assign stall = any_instr_in_pipe;
+   
+   wire                            execute_is_branch;
+   branch_detector execute_b(.instr(execute_stage_instruction),
+                            .is_branch(execute_is_branch));
+
+   wire                            memory_is_branch;
+   branch_detector memory_b(.instr(memory_stage_instruction),
+                            .is_branch(memory_is_branch));
+   
+   wire                            any_branch_past_issue;
+   assign any_branch_past_issue = decode_is_branch ||
+                                  execute_is_branch ||
+                                  memory_is_branch;
+
+   wire                            any_RAW_dependence;
+   wire                            issued_i_reads_reg_0;
+   wire                            issued_i_reads_reg_1;
+   wire [4:0]                      issued_i_reg_0;
+   wire [4:0]                      issued_i_reg_1;
+   
+   assign stall = any_branch_past_issue || any_RAW_dependence;
+   
+   // wire any_instr_in_pipe = (32'h0 != decode_stage_instruction) ||
+   //      (32'h0 != execute_stage_instruction) ||
+   //      (32'h0 != memory_stage_instruction);
+
+   // assign stall = any_instr_in_pipe;
 
 endmodule
