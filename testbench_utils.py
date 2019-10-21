03c74526f9a2d798613aabc3105b84214a7e7d98
Joachim Lusiardi <joachim@lusiardi.de>
2019-10-21 20:13:19 +0200

    Changed print to function

--- testbench_utils.py
+++ testbench_utils.py
@@ -4,7 +4,7 @@ def os_cmd(command_string):
     res = os.system(command_string)
 
     if (res != 0):
-        print 'FAILED COMMAND: ', command_string
+        print('FAILED COMMAND: ', command_string)
         assert(False)
 
 # Module by module tests
@@ -14,7 +14,7 @@ def build_module_with_main(mod_name, main_name, flags):
     verilate = os.system(v_command);
 
     if (verilate != 0):
-        print 'ERROR: ' + mod_name + ' verilation failure',
+        print('ERROR: ' + mod_name + ' verilation failure')
         assert(False)
 
     m_command = "make -C obj_dir -j -f V" + mod_name + ".mk V" + mod_name
@@ -22,13 +22,13 @@ def build_module_with_main(mod_name, main_name, flags):
     make_cmd = os.system(m_command)
 
     if (make_cmd != 0):
-        print 'ERROR: ' + mod_name + ' could not make verilated code',
+        print('ERROR: ' + mod_name + ' could not make verilated code')
         assert(False)
 
     run_cmd = os.system('./obj_dir/V' + mod_name)
 
     if (run_cmd != 0):
-        print 'ERROR: ' + mod_name + ' tests failed'
+        print('ERROR: ' + mod_name + ' tests failed')
         assert(False)
     
 def build_module(mod_name, flags=''):
